b31a9ef4b5670536756572ded1efc997d4878204
Erik <eriksantos617@gmail.com>
2020-08-21 15:42:29 -0300

    Commit 1

---
+++ POO_ClassePessoa/Pessoa.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace POO_ClassePessoa
+{
+    class Pessoa
+    {
+        private string _nome;
+        private string _endereco;
+        private string _telefone;
+
+        public string GetNome()
+        {
+            return _nome;
+        }
+
+        public void setNome(string nome)
+        {
+            _nome = nome;
+        }
+
+        public string GetEndereco()
+        {
+            return _endereco;
+        }
+
+        public void setEndereco(string endereco)
+        {
+            _endereco = endereco;
+        }
+
+        public string GetTelefone()
+        {
+            return _telefone;
+        }
+
+        public void setTelefone(string telefone)
+        {
+            _telefone = telefone;
+        }
+
+        public Pessoa(string nome, string endereco, string telefone)
+        {
+            _nome = nome;
+            _endereco = endereco;
+            _telefone = telefone;
+        }
+
+        public override string ToString()
+        {
+            return 
+                "Nome: " + GetNome() +
+                "\nEndereco : " + GetEndereco() +
+                "\nTelefone: " + GetTelefone() + 
+                "\n" ;
+        }
+    }
+}
---
+++ POO_ClassePessoa/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace POO_ClassePessoa
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Quantidade de pessoas: ");
+            int quant = int.Parse(Console.ReadLine());
+
+            List<Pessoa> p1 = new List<Pessoa>();
+
+            for (int i = 0; i < quant; i++)
+            {
+                Console.Write("\nNome: ");
+                string nome = Console.ReadLine();
+
+                Console.Write("Endereco: ");
+                string endereco = Console.ReadLine();
+
+                Console.Write("telefone: ");
+                string telefone = Console.ReadLine();
+
+                p1.Add(new Pessoa(nome, endereco, telefone));
+            }
+
+            Console.Clear();
+
+            foreach (var p in p1)
+            {
+                Console.WriteLine(p);
+            }
+        }
+    }
+}
